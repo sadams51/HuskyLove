bc9d049ed33ae953125aa7f84589d5ce6213dba3
Sarah Adams <adams.sarahn@gmail.com>
2018-01-16 14:18:07 -0600

    validations added "isIn"

--- models/dogs.js
+++ models/dogs.js
@@ -1,16 +1,62 @@
+
+//model definition 
 module.exports = function(sequelize, DataTypes) {
   var Dogs = sequelize.define("Dogs", {
-    eyeColorOne: {type: DataTypes.STRING,validate: {len: [1, 1]}},
-    eyeColorTwo: {type: DataTypes.STRING,validate: {len: [1, 1]}},
-    coatColorOne: {type: DataTypes.STRING,validate: {len: [1, 1]}},
-    coatColorTwo: {type: DataTypes.STRING,validate: {len: [1, 1]}},
-    tailLengthOne: {type: DataTypes.STRING,validate: {len: [1, 1]}},
-    tailLengthTwo: {type: DataTypes.STRING,validate: {len: [1, 1]}},    
-    tongueOne: {type: DataTypes.STRING,validate: {len: [1, 1]}},
-    tongueTwo: {type: DataTypes.STRING,validate: {len: [1, 1]}},    
-    sexOne: {type: DataTypes.STRING,validate: {len: [1, 1]}},
-    sexTwo: {type: DataTypes.STRING,validate: {len: [1, 1]}},    
-    genoType: {type: DataTypes.STRING,validate: {len: [8, 8]}}
+    eyeColorOne: {type: DataTypes.STRING,
+        validate: {
+            len: [1, 1],
+            isIn: [['B', 'b']],
+            notEmpty: true
+        }},
+    eyeColorTwo: {type: DataTypes.STRING,
+        validate: {
+            len: [1, 1],
+            isIn: [['B', 'b']],
+            notEmpty: true
+        }},
+    coatColorOne: {type: DataTypes.STRING,
+        validate: {
+            len: [1, 1],
+            isIn: [['G', 'W']],
+            notEmpty: true
+        }},
+    coatColorTwo: {type: DataTypes.STRING,
+        validate: {
+            len: [1, 1],
+            isIn: [['G', 'W']],
+            notEmpty: true
+        }},
+    tailLengthOne: {type: DataTypes.STRING,
+        validate: {
+            len: [1, 1],
+            isIn: [['L', 'S']]
+        }},
+    tailLengthTwo: {type: DataTypes.STRING,
+        validate: {
+            len: [1, 1],
+            isIn: [['L', 'S']]
+        }},    
+    tongueOne: {type: DataTypes.STRING,
+        validate: {
+            len: [1, 1],
+            isIn: [['T', 't']]
+        }},
+    tongueTwo: {type: DataTypes.STRING,
+        validate: {
+            len: [1, 1],
+            isIn: [['T', 't']]
+        }},    
+    sexOne: {type: DataTypes.STRING,
+        validate: {
+            len: [1, 1],
+            isIn: [['X']]
+        }},
+    sexTwo: {type: DataTypes.STRING,
+        validate: {
+            len: [1, 1],
+            isIn: [['X', 'Y']]
+        }},    
+    genoType: {type: DataTypes.STRING,validate: {len: [8, 8], is: ["^[a-z]+$", 'i']}},
     huskyImage: {type: DataTypes.STRING}
   });
   return Dogs;
