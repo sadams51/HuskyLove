df35638b5079140a8fb2233aaf2695dcdd8aa039
Sarah Adams <adams.sarahn@gmail.com>
2018-01-20 11:24:49 -0600

    routes are confusing

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -3,12 +3,12 @@ var models = require("../../app/models");
 
 var authController = require('../controllers/authcontroller.js');
 
-// module.exports = function(app, passport) {
+module.exports = function(app, passport) {
 
-// 	app.get('/login', authController.login);
+	app.get('/login', authController.login);
 
-// 	app.post('/login', passport.authenticate('local-login', {
-// 			successRedirect: '/',
+	app.post('/login', passport.authenticate('local-login', {
+			successRedirect: '/',
 
 
 			failureRedirect: '/login'
@@ -16,25 +16,25 @@ var authController = require('../controllers/authcontroller.js');
 	));	
 
 
-// //if you run the app & try to visit the dashboard & you aren't logged in, you will be 
-// //redirected to the sign-in page 
-// 	// app.get('/dashboard', isLoggedIn, authController.dashboard);	
+//if you run the app & try to visit the dashboard & you aren't logged in, you will be 
+//redirected to the sign-in page 
+	app.get('/dashboard', isLoggedIn, authController.dashboard);	
 
 
 
-// 	// ));
+	));
 
 
-// 	// app.get('/logout', authController.logout);
+	app.get('/logout', authController.logout);
 
 
 
-// 	function isLoggedIn(req, res, next) {
-// 		if (req.isAuthenticated())
-// 			return next();
+	function isLoggedIn(req, res, next) {
+		if (req.isAuthenticated())
+			return next();
 
-// 		res.redirect('/login');
-// 	}
+		res.redirect('/login');
+	}
 
 
-// }
+}
